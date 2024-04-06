2c3b527ed0df54c9fd76fcb40bc815beea3118f7
Goutam duggad <165892975+Goutamduggad@users.noreply.github.com>
2024-04-06 16:07:09 +0530

    Update form(main).py
    Adding-Feedback Questions

--- form(main).py
+++ form(main).py
@@ -46,3 +46,11 @@ if(faculty.isdigit()):
 sem=input("semester:")
 
 #add feedback questions with input statement
+Are you satisfied with the overall teaching of the Course*=input("Are you satisfied with the overall teaching of the Course:")
+Subject knowledge of the Course Lead*=input("Subject knowledge of the Course Lead:")
+Communication skills of the Course Lead*=input("Communication skills of the Course Lead:")
+Lectures were delivered at a pace that is*=input("Lectures were delivered at a pace that is:")
+Would you like to have another course with the same Course Lead*=input("Would you like to have another course with the same Course Lead:")
+Overall rating for this course*=input("Overall rating for this course:")
+The course was organised in a way that helped me to learn*=input("The course was organised in a way that helped me to learn:")
+Overall rating for the Course Lead*=input("Overall rating for the Course Lead:")
